d990a91566fbf932e4c1d554c5caa2937b40e19c
takuma sakimae <true.under.the.tree.bad.end@gmail.com>
2017-10-17 08:40:52 +0900

    とりま

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+
+//リクエストを叩くための設定（だったような）
 var request = require('request');
 var headers = {
     'Content-Type': 'application/json'
 }
+
 const Botkit = require('botkit');
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
     process.exit(1);
 }
-
+//データを保存するファイルの指定
 const controller = Botkit.slackbot({
     json_file_store: 'storage_bot_db'
 });
-
+//起動時の設定（だろう）　いじらなくていい所
 controller.spawn({
     token: process.env.token
 }).startRTM(function (err) {
@@ -20,7 +23,16 @@ controller.spawn({
         throw new Error(err);
     }
 });
+
+
 //あとでモジュール化する関数
+/**
+ * @classdesc スレッドデータを取得する。Promise
+ * @param {obj} channel_data データストアに保存してある朝会の情報 {id,ts,time}
+ * @param {obj} bot　bot機能のインスタンス？よくわからん 
+ * @return  messages 
+ * 
+ */
 function getThread(channel_data, bot) {
     return new Promise(function (resolve, reject) {
         bot.api.channels.replies({
@@ -37,6 +49,98 @@ function getThread(channel_data, bot) {
     });
 }
 
+
+//asakai start 書き直し
+controller.hears(['朝会始め', '--start'], 'direct_mention', function (bot, message) {
+    controller.storage.channels.get(message.channel, function (err, channel_data) {
+        if (channel_data) {
+            bot.say({
+                text: "朝会はすでに始まっています",
+                channel: message.channel
+            });
+        } else {
+            let time = new Date();
+            bot.say({
+                text: '<@here> 本日（' + time + '）の朝会スレッドはこちら',
+                channel: message.channel
+            }, function (err, res) {
+                bot.api.pins.add({
+                    channel:res.channel,
+                    timestamp:res.ts
+                });
+                controller.storage.channels.save({
+                    id: message.channel, time: time, ts: res.ts
+                }, function (err) {
+                    if (err) {
+                        bot.say({
+                            text: "データストアの利用に失敗",
+                            channel: message.channel
+                        });
+                    }
+                });
+            });
+        }
+    });
+});
+//asakai end 書き直し
+controller.hears(['朝会終わり', '--end'], 'direct_mention', function (bot, message) {
+    //チャンネルID.jsonに保存しているデータを取り出す
+    controller.storage.channels.get(message.channel, function (err, channel_data) {
+        if (!channel_data) {
+            bot.say({
+                text: '朝会はまだ始まっていません',
+                channel: message.channel
+            });
+        } else {
+            bot.say({
+                text: '朝会を終了しました。本日の朝会時間：' + (new Date() - new Date(channel_data.time)) + 'ms',
+                channel: message.channel
+            });
+            //データ取得
+            var thread = getThread(channel_data, bot)
+            .then(function (messages) {
+                if (messages[0].reply_count) {
+                    var threadMessage = '';
+                    messages.forEach(function (element) {
+                        console.log(JSON.stringify(element));
+                        threadMessage = threadMessage + element.text + '¥n';
+                        /*
+                        threadMessage.push({
+                            text: element.text,
+                            user: element.user
+                        });
+                        */
+                    });
+                    return threadMessage;
+                } else {
+                    bot.say({
+                        text: "スレッドに対するリプライはありません",
+                        channel: message.channel
+                    });
+                    return;
+                }
+            }).then(function(thread_message){
+                bot.say({
+                    text: thread_message,
+                    channel:message.channel
+                });
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+            
+            controller.storage.channels.delete(message.channel, function (err) {
+                if (err) {
+                    bot.say({
+                        text: 'このデータストアがやばい！2017',
+                        channel: message.channel
+                    });
+                }
+            });
+            
+        }
+    });
+});
 
 /**
  * 情報確認用テスト
@@ -77,118 +181,3 @@ controller.hears('gettest', ['direct_message', 'direct_mention', 'mention'], fun
         });
     });
 });
-
-//asakai start 書き直し
-controller.hears(['朝会始め', '--start'], 'direct_mention', function (bot, message) {
-    controller.storage.channels.get(message.channel, function (err, channel_data) {
-        if (channel_data) {
-            bot.say({
-                text: "朝会はすでに始まっています",
-                channel: message.channel
-            });
-        } else {
-            let time = new Date();
-            bot.say({
-                text: '@here 本日（' + time + '）の朝会スレッドはこちら',
-                channel: message.channel
-            }, function (err, res) {
-                controller.storage.channels.save({
-                    id: message.channel, time: time, ts: res.ts
-                }, function (err) {
-                    if (err) {
-                        bot.say({
-                            text: "データストアの利用に失敗",
-                            channel: message.channel
-                        });
-                    }
-                });
-            });
-        }
-    });
-});
-//asakai end 書き直し
-controller.hears(['朝会終わり', '--end'], 'direct_mention', function (bot, message) {
-    controller.storage.channels.get(message.channel, function (err, channel_data) {
-        if (!channel_data) {
-            bot.say({
-                text: '朝会はまだ始まっていません',
-                channel: message.channel
-            });
-        } else {
-            bot.say({
-                text: '朝会を終了しました。本日の朝会時間：' + (new Date() - new Date(channel_data.time)) + 'ms',
-                channel: message.channel
-            });
-            //データ取得
-            var thread = getThread(channel_data, bot)
-                .then(function (messages) {
-                    if (messages[0].reply_count) {
-                        var threadMessage = '';
-                        messages.forEach(function (element) {
-                            threadMessage.concat(element.text).concat('¥r¥n');
-                            /*
-                            threadMessage.push({
-                                text: element.text,
-                                user: element.user
-                            });
-                            */
-                        });
-                        return threadMessage;
-                    } else {
-                        bot.say({
-                            text: "スレッドに対するリプライはありません",
-                            channel: message.channel
-                        });
-                        return;
-                    }
-                })
-                .catch(function (err) {
-                    console.log(err);
-                });
-            console.log(thread + "+");
-
-            controller.storage.channels.delete(message.channel, function (err) {
-                if (err) {
-                    bot.say({
-                        text: 'このデータストアがやばい！2017',
-                        channel: message.channel
-                    });
-                }
-            });
-
-        }
-    });
-});
-/**
- * スレッドからデータを引っ張ってくるサンプル
-controller.hears('-o', 'direct_mention', function (bot, message) {
-    if (start) {
-        end = new Date();
-        //bot.reply(message,asakaiEndMsg1 + (endTime - startTime) + asakaiEndMsg2); 
-        //このタイミングでスレッドの取得を行いたい
-        //つまりAPIを叩くということだと思うのだが...?
-        bot.api.channels.replies({
-            token: process.env.token,
-            channel: start.channel,
-            thread_ts: start.ts
-        }, function (err, res) {
-            var arr = res.messages;
-            arr.forEach(function (element) {
-                console.log(element.text);
-            });
-        });
-
-
-        bot.say({
-            text: asakaiEndMsg1 + (end - start.time) + asakaiEndMsg2,
-            channel: message.channel
-        });
-        start = null;
-    } else {
-        bot.say({
-            text: "朝会始まってなくね？",
-            channel: message.channel
-        });
-    }
-});
- */
